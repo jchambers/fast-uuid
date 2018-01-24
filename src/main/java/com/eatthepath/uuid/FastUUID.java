5569326796ca7a5f268688530ad8670e2b3ff574
Jon Chambers <jon@turo.com>
2018-01-23 16:18:06 -0800

    Look up hex values in an array instead of continually recalculating them.

--- src/main/java/com/eatthepath/uuid/FastUUID.java
+++ src/main/java/com/eatthepath/uuid/FastUUID.java
@@ -42,6 +42,39 @@ public class FastUUID {
     private static final char[] HEX_DIGITS =
             new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
+    private static final int[] HEX_VALUES = new int[128];
+
+    static {
+        for (int i = 0; i < HEX_VALUES.length; i++) {
+            HEX_VALUES[i] = -1;
+        }
+
+        HEX_VALUES['0'] = 0x0;
+        HEX_VALUES['1'] = 0x1;
+        HEX_VALUES['2'] = 0x2;
+        HEX_VALUES['3'] = 0x3;
+        HEX_VALUES['4'] = 0x4;
+        HEX_VALUES['5'] = 0x5;
+        HEX_VALUES['6'] = 0x6;
+        HEX_VALUES['7'] = 0x7;
+        HEX_VALUES['8'] = 0x8;
+        HEX_VALUES['9'] = 0x9;
+
+        HEX_VALUES['a'] = 0xa;
+        HEX_VALUES['b'] = 0xb;
+        HEX_VALUES['c'] = 0xc;
+        HEX_VALUES['d'] = 0xd;
+        HEX_VALUES['e'] = 0xe;
+        HEX_VALUES['f'] = 0xf;
+
+        HEX_VALUES['A'] = 0xa;
+        HEX_VALUES['B'] = 0xb;
+        HEX_VALUES['C'] = 0xc;
+        HEX_VALUES['D'] = 0xd;
+        HEX_VALUES['E'] = 0xe;
+        HEX_VALUES['F'] = 0xf;
+    }
+
     private FastUUID() {
         // A private constructor prevents callers from accidentally instantiating FastUUID instances
     }
@@ -187,14 +220,14 @@ public class FastUUID {
     }
 
     static int getHexValueForChar(final char c) {
-        if (c >= '0' && c <= '9') {
-            return c - 48;
-        } else if (c >= 'a' && c <= 'f') {
-            return c - 87;
-        } else if (c >= 'A' && c <= 'F') {
-            return c - 55;
-        } else {
+        try {
+            if (HEX_VALUES[c] < 0) {
+                throw new IllegalArgumentException("Illegal hexadecimal digit: " + c);
+            }
+        } catch (final ArrayIndexOutOfBoundsException e) {
             throw new IllegalArgumentException("Illegal hexadecimal digit: " + c);
         }
+
+        return HEX_VALUES[c];
     }
 }
